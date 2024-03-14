87241a1b66819ace92bacc003bf624387fbf2ebb
oabdelnour <olivier.abdelnour@gmail.com>
2024-03-14 14:21:10 +0100

    add virtual list and masonry

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
 import './App.css';
 import { css } from '../styled-system/css';
-import { DraggableModal } from './component/ModalDrag/DraggableModal.tsx';
-import { useState } from 'react';
-import { Button } from 'antd';
+import { MasonryTest } from './component/VirtualListTest/MasonryTest.tsx';
+import { VirtualListTest } from './component/VirtualListTest/VirtualListTest.tsx';
 
 function App() {
-    const [counter, setCounter] = useState<number>(0);
     return (
         <div className={css({ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' })}>
-            <Button onClick={() => setCounter(counter + 1)}>{counter}</Button>
-            <DraggableModal isDraggable isResizable title="COucou" enableBackground >
-                COucou COucou COucou COucou COucou COucou COucou COucou COucou COucou v COucou COucou COucou COucou v v
-                v COucouCOucouCOucouCOucouCOucouCOucouCOucouCOucou
-
-                COucou COucou COucou COucou COucou COucou COucou COucou COucou COucou v COucou COucou COucou COucou v v
-                v COucouCOucouCOucouCOucouCOucouCOucouCOucouCOucou
-
-                COucou COucou COucou COucou COucou COucou COucou COucou COucou COucou v COucou COucou COucou COucou v v
-                v COucouCOucouCOucouCOucouCOucouCOucouCOucouCOucou
-
-                COucou COucou COucou COucou COucou COucou COucou COucou COucou COucou v COucou COucou COucou COucou v v
-                v COucouCOucouCOucouCOucouCOucouCOucouCOucouCOucou
-
-                COucou COucou COucou COucou COucou COucou COucou COucou COucou COucou v COucou COucou COucou COucou v v
-                v COucouCOucouCOucouCOucouCOucouCOucouCOucouCOucou
-            </DraggableModal>
+            <MasonryTest />
         </div>
     );
 }
