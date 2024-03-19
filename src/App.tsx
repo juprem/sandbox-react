ad43d14e02ef893c0ac519244cc7d8c5f788e6ce
oabdelnour <olivier.abdelnour@gmail.com>
2024-03-19 15:12:43 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css';
 import { css } from '../styled-system/css';
-import { CarouselSlick } from './component/CarouselSlick/CarouselSlick.tsx';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { MasonryTest } from './component/VirtualListTest/MasonryTest.tsx';
 
 function App() {
     return (
         <div className={css({ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' })}>
             <div style={{ width: '300px' }}>
-                <CarouselSlick />
+                <MasonryTest />
             </div>
         </div>
     );
