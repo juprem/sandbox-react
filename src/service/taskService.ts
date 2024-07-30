b69379540e3b463885733c510ae412beaa31c691
oabdelnour <olivier.abdelnour@gmail.com>
2024-07-30 16:44:23 +0200

    feat: improve zod function, add test on useReducer

--- src/service/taskService.ts
+++ src/service/taskService.ts
@@ -5,16 +5,12 @@ export function taskService() {
     const { getQuery, putQuery } = fetchService();
 
     async function getTaskByTodo(todoId: string) {
-        const tasks = await getQuery(`/todos/${todoId}/tasks`);
-
-        return TasksSchema.parse(tasks);
+        return await getQuery(`/todos/${todoId}/tasks`, TasksSchema);
     }
 
     async function putTask(task: Task) {
         const { id, ...updateTask } = task;
-        const updatedTask = await putQuery(`/tasks/${id}`, updateTask);
-
-        return TaskSchema.parse(updatedTask);
+        return await putQuery(`/tasks/${id}`, updateTask, TaskSchema);
     }
 
     return { getTaskByTodo, putTask };
