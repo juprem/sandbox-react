38143f7a12bbdfc76d8f643ce4999c59edbcb1b1
oabdelnour <olivier.abdelnour@gmail.com>
2025-01-07 13:55:26 +0100

    fix: memory history pages

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ZodError } from 'zod';
 import { css } from '@styled-system/css';
-import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { createMemoryHistory, createRouter, RouterProvider } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
 import { ConfigProvider } from 'antd';
 import dayjs from 'dayjs';
@@ -26,8 +26,13 @@ const queryClient = new QueryClient({
     },
 });
 
+const memoryHistory = createMemoryHistory({
+    initialEntries: ['/'], // Pass your initial url
+});
+
 const router = createRouter({
     routeTree,
+    history: memoryHistory,
     context: {
         queryClient: queryClient,
         breadcrumbs: '',
@@ -41,7 +46,7 @@ declare module '@tanstack/react-router' {
     }
 }
 
-dayjs.locale('fr')
+dayjs.locale('fr');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <StrictMode>
@@ -50,7 +55,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
                 token: {
                     fontFamily: 'monospace',
                     colorText: 'white',
-                    colorBgBase: '#2e2e2e'
+                    colorBgBase: '#2e2e2e',
                 },
             }}
         >
